3a4bf22d8e6ba961c02c9ba2eb25a9c7bd67c633
Abhishek Satyal <ab.satyal@gmail.com>
2024-01-24 22:53:38 -0500

    feat: init project

---
+++ src/test/kotlin/com/whiteKnightz/theater/TheaterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.whiteKnightz.theater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheaterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
